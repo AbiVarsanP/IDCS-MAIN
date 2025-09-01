5119c87a827c26f8172378b5e97172eecebaf96b
unknown <rexciaalex@gmail.com>
2025-09-01 11:43:16 +0530

    HOD DELETE

--- core/urls.py
+++ core/urls.py
@@ -24,6 +24,11 @@ urlpatterns = [
     path("dash/", ahod_dash, name="ahod_dash"),
     path('student/timetable/', student_timetable, name='student_timetable'),
     path("ahod/", include("core.ahod_urls")),
+    path('hod/notifications/delete_all/', delete_all_hod_notifications, name='delete_all_hod_notifications'),
+    path('hod/notifications/history/', hod_notification_history, name='hod_notification_history'),
+
+    path('hod/notifications/delete_all/', delete_all_notifications, name='delete_all_notifications'),
+    path('hod/notifications/history/', ahod_notification_history, name='hod_notification_history'),
 
 
 ]

d21a5a5d48358efcc56d09efbb422934de5019d0
Rexcia21 <rexciaalex@gmail.com>
2025-09-23 14:27:28 +0530

    hod timetable added

--- core/timetable_views.py
+++ core/timetable_views.py
@@ -41,3 +41,39 @@ def staff_timetable(request):
     context['table'] = table
     context['my_table'] = my_table
     return render(request, 'staff/timetable.html', context)
+
+@login_required
+def hod_timetable(request):
+    context = set_config(request)
+    days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+    periods = ['1', '2', '3', '4', '5', '6', '7']
+    hod = context['duser']
+    table = {}
+    my_table = {}
+    try:
+        timetable_obj = StaffTimeTable.objects.get(staff=hod)
+        table = timetable_obj.data
+        my_table = timetable_obj.my_timetable_data
+    except StaffTimeTable.DoesNotExist:
+        timetable_obj = None
+    if request.method == 'POST':
+        if 'my-edit-btn' in request.POST or 'my-save-btn' in request.POST or any(k.startswith('my_') for k in request.POST.keys()):
+            for day in days:
+                for period in periods:
+                    key = f"{day}_{period}"
+                    my_key = f"my_{key}"
+                    my_table[key] = request.POST.get(my_key, '')
+            StaffTimeTable.objects.update_or_create(staff=hod, defaults={'my_timetable_data': my_table, 'data': table})
+            context['my_message'] = 'My Timetable updated!'
+        else:
+            for day in days:
+                for period in periods:
+                    key = f"{day}_{period}"
+                    table[key] = request.POST.get(key, '')
+            StaffTimeTable.objects.update_or_create(staff=hod, defaults={'data': table, 'my_timetable_data': my_table})
+            context['message'] = 'Timetable updated!'
+    context['days'] = days
+    context['periods'] = periods
+    context['table'] = table
+    context['my_table'] = my_table
+    return render(request, 'hod/timetable.html', context)
